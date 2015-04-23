f11c0770bc427979a544a47e7ae156f091ebf5d7
Jason Bandlow <jbandlow@gmail.com>
2015-04-22 21:39:30 -0700

    Added some styling and some text

--- meme-voter/meme-voter.js
+++ meme-voter/meme-voter.js
@@ -12,45 +12,72 @@ if (Meteor.isClient) {
   // Ballot Template
   //    Helpers
   Template.ballot.helpers({
-      imageUrl: function() { return this.url || ''; }
-  });
-
-  Template.ballot.helpers({
+      imageUrl: function() { return this.url || ''; },
       imageScore: function() { return this.score || 0; }
   });
 
   //    Event Handlers
-  // TODO: Voting
+  Template.ballot.events({
+    'click .upvote': function(event, template) {
+      Images.update(this._id, {'$inc': {'score': 1}});
+    }
+  });
+  Template.ballot.events({
+    'click .downvote': function(event, template) {
+      console.log(template);
+      Images.update(this._id, {'$inc': {'score': -1}});
+    }
+  });
+
+  Template.ballot.onRendered(function() {
+    console.log('ballot: ',this);
+  });
+
+  Template.meme.onRendered(function() {
+    console.log('meme: ',this);
+  });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function() {
-    if (Images.find().count() == 0) {
+    Images.remove({});
+    //if (Images.find().count() == 0) {
       var initial_images = [
         {
           'name' : 'Wonka',
-          'url' : 'http://images.memegenerator.net/images/298x/1031.jpg'
+          'url' : 'http://images.memegenerator.net/images/298x/1031.jpg',
+          'score': 0
         },
         {
           'name' : 'Success Kid',
-          'url' : 'http://images.memegenerator.net/images/298x/2729805.jpg'
+          'url' : 'http://images.memegenerator.net/images/298x/2729805.jpg',
+          'score': 0
         },
         {
           'name' : 'Boromir',
-          'url' : 'http://images.memegenerator.net/images/298x/3291562.jpg'
+          'url' : 'http://images.memegenerator.net/images/298x/3291562.jpg',
+          'toptext': 'One does not simply',
+          'bottomtext': 'build reactive webapps',
+          'score': 0
         },
         {
           'name' : 'First World Problems',
-          'url' : 'http://images.memegenerator.net/images/298x/2055789.jpg'
+          'url' : 'http://images.memegenerator.net/images/298x/2055789.jpg',
+          'toptext': "I can't keep my server",
+          'bottomtext': "in sync with my client",
+          'score': 0
         },
         {
           'name' : 'Philosoraptor',
-          'url' : 'http://images.memegenerator.net/images/298x/984.jpg'
+          'url' : 'http://images.memegenerator.net/images/298x/984.jpg',
+          'toptext': "What if my framework",
+          'bottomtext': "managed state?",
+          'score': 0
         }
       ];
       _.each(initial_images, function(img) {
           Images.insert(img);
       });
-    }
+    //}
   });
 }
